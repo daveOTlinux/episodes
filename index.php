48a72982abcf40b9b8e11cbe203bbc39ba60ad38
Dave Mason <onthebay.dave@gmail.com>
2018-07-02 13:52:00 -0300

    Must use non-slim version of jquery for search box to work.

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
 	<title>Episodes Dashboard</title>
-	<script src="jquery.js"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--	<script src="jquery.js"></script> -->
+	<script
+		src="https://code.jquery.com/jquery-3.3.1.min.js"
+		integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+		crossorigin="anonymous"></script>
+	<link
+		rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+		integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+		crossorigin="anonymous">
+  	<script
+  		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+  		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+  		crossorigin="anonymous"></script>
+	<script
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+		integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+		crossorigin="anonymous"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
 	<link href="style.css" rel="stylesheet">
 	
 	<script>
@@ -52,7 +67,7 @@
 <body>
     <div class="wrapper">
         <div class="container-fluid">
-			<div class="row" style="height: 100px;>
+			<div class="row" style="height: 75px;">
 				<div class="col-sm-6">
 					<div class="page-header clearfix">
 						<h2 class="pull-left">Show Episodes</h2>
@@ -62,7 +77,7 @@
 					<a href="create.php" class="btn btn-success pull-right">Add New Show</a>
 				</div>
 			</div>
-           <div class="row">    
+           <div class="row" style="height: 50px;">    
 					<div class="col-sm-6">
 					   <div class="dropdown">
 					        <a data-target="#" href="index.php" data-toggle="dropdown" class="dropdown-toggle">Sort By <b class="caret"></b></a>
@@ -98,12 +113,12 @@
                       echo "<table class='table table-bordered table-hover'>";
                           echo "<thead>";
                               echo "<tr>";
-                                  echo "<th style='width: 10%'>ID</th>";
+                                  echo "<th style='width: 8%'>ID</th>";
                                   echo "<th style='width: 40%'>Show Name</th>";
                                   echo "<th style='width: 5%'>Next Episode</th>";
                                   echo "<th style='width: 5%'>Season</th>";
                                   echo "<th style='width: 20%'>Show Source</th>";
-                                  echo "<th style='width: 20%'>Action</th>";
+                                  echo "<th style='width: 22%'>Action</th>";
                               echo "</tr>";
                           echo "</thead>";
                           echo "<tbody>";
@@ -115,9 +130,9 @@
                                   echo "<td>" . $row['Season'] . "</td>";
                                   echo "<td>" . $row['ShowSource'] . "</td>";
                                   echo "<td>";
-                                      echo "<a href='read.php?ID=". $row['ID'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                      echo "<a href='update.php?ID=". $row['ID'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                      echo "<a href='delete.php?ID=". $row['ID'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                      echo "<a href='read.php?ID=". $row['ID'] ."' title='View Record' data-toggle='tooltip'><span class='fa fa-eye'></span></a>";
+                                      echo "<a href='update.php?ID=". $row['ID'] ."' title='Update Record' data-toggle='tooltip'><span class='fa fa-pencil'></span></a>";
+                                      echo "<a href='delete.php?ID=". $row['ID'] ."' title='Delete Record' data-toggle='tooltip'><span class='fa fa-trash'></span></a>";
                                   echo "</td>";
                               echo "</tr>";
                           }
